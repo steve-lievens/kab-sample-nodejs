f30522e2752a8239712cc9defc849f95a9a91173
steve.lievens <steve.lievens@be.ibm.com>
2019-09-24 14:28:53 +0200

    App update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')()
 
 app.get('/', (req, res) => {
   var message = "";
-  message = "Hello from the Appsody Nodejs demo !!!!!!!!";
+  message = "Hello from the Appsody Nodejs demo !!!!!!!! Time to deploy it with Tekton !!!!";
   
   res.send(message);
 });
