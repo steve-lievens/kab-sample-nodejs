a3a8d939624cabecb19e24d1b5febc194133958b
steve.lievens <steve.lievens@be.ibm.com>
2019-09-24 13:56:49 +0200

    Whitespace removal

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const app = require('express')()
 
 app.get('/', (req, res) => {
   var message = "";
-
   message = "Hello from the Appsody Nodejs demo !!!!!!!!";
   
   res.send(message);
-
-
 });
  
 module.exports.app = app;
