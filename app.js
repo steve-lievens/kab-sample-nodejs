5cff8f1cd1e1299e1b0df8fe4a1b54d745d084f0
steve.lievens <steve.lievens@be.ibm.com>
2019-10-01 18:53:15 +0200

    Switch from message to UI

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const app = require('express')()
+var express = require("express");
+const app = express();
+var os = require("os");
+var hostname = os.hostname();
 
-app.get('/', (req, res) => {
-  var message = "";
-  message = "Now for something completely different ...";
-  
-  res.send(message);
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
+
+var REGION = process.env.REGION;
+
+console.log("INFO: REGION", REGION);
+
+// --------------------------------------------------------------------------
+// REST API : retrieve info about the host
+// --------------------------------------------------------------------------
+app.get('/gethostinfo', function(req, res) {
+  var hostobj = {
+    "hostname": hostname,
+    "region": REGION
+  }
+  console.log("Service gethostinfo returning " + JSON.stringify(hostobj));
+  res.json(hostobj);
 });
+
  
 module.exports.app = app;
